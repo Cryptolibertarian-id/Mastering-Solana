cfc478aed0755246b1b792c9c89bb6d43411c49d
Cryptolibertarian.id <hazekaizer@gmail.com>
2022-07-10 11:29:05 +0700

    update script

--- src/airdrop-project/index.js
+++ src/airdrop-project/index.js
@@ -1,2 +1,6 @@
-const { Keypair } = require("@solana/web3.js");
-console.log(new Keypair());
+const { Keypair, PublicKey } = require("@solana/web3.js");
+const wallet = new Keypair();
+console.log(wallet.publicKey.toBase58());
+console.log(new PublicKey());
+console.log(wallet.secretKey);
+//console.log(new Keypair());
